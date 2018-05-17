a073909f9adeef95549b7ba541c6aeedd96da3c2
Riley Cravens <rlycrvns@gmail.com>
2018-05-17 12:32:40 -0700

    fixed hamburger animation

--- includes/scripts.inc.php
+++ includes/scripts.inc.php
@@ -3,3 +3,4 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
 <script src="js/scripts.js"></script>
+<script src="js/hamburger.js"></script>
