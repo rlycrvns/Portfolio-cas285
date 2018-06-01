4777992f111304d47e7d8bad585291533f965cee
Riley Cravens <rlycrvns@gmail.com>
2018-06-01 14:23:26 -0700

    put name and email fields in the form into columns to be inline in desktop

--- php/form.php
+++ php/form.php
@@ -1,21 +1,19 @@
-
 <form method="post" action="  " id="inquiryForm">
-  <div class="form-group">
-   <label for="name">Name:</label>
-    <input class="form-control" type="text" name="name" id="name" type="text" required>
-  </div>
+  <div class="row">
+    <div class="form-group col-md">
+      <label for="name">Name:</label>
+      <input class="form-control" type="text" name="name" id="name" type="text" required>
+    </div>
 
-  <div class="form-group">
-    <label for="email">Email:</label>
-    <small id="emailHelp" class="form-text text-muted">I will never share your email with anyone else.</small>
-    <input class="form-control" type="email" name="email" id="email" required>
-  </div>
-
-  <div class="form-group">
-    <label for="subject">Subject:</label>
-    <input class="form-control" type="text" name="subject" id="subject" required>
-  </div>
-
+    <div class="form-group col-md">
+      <label for="email">Email:</label>
+      <input class="form-control" type="email" name="email" id="email" placeholder="I will never share your email with anyone else." required>
+    </div>
+</div>
+    <div class="form-group">
+      <label for="subject">Subject:</label>
+      <input class="form-control" type="text" name="subject" id="subject" required>
+    </div>
   <div class="form-group">
     <label for="message"></label>
     <textarea class="form-control" name="message" id="message" rows="5" placeholder="Message..." required></textarea>
@@ -26,7 +24,7 @@
   <br>
 </form>
 <script>
-$(document).ready(function () {
-  $("#inquiryForm").validate();
-});
+  $(document).ready(function() {
+    $("#inquiryForm").validate();
+  });
 </script>
