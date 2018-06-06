6853474243d8ff0135d06b9f403c5aeda5d018e4
Riley Cravens <rlycrvns@gmail.com>
2018-06-06 13:20:33 -0700

    linked minified versions of js files

--- includes/scripts.inc.php
+++ includes/scripts.inc.php
@@ -3,6 +3,6 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
-<script src="js/hamburger.js"></script>
-<script src="js/jquery.gridder.js"></script>
+<script src="js/hamburger-min.js"></script>
+<script src="js/jquery.gridder-min.js"></script>
 <script src="js/scripts.js"></script>
